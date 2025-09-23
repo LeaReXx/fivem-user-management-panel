221b9c32c4e07cc1d09af0ab6f2273cef68e6678
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-09-23 15:04:45 +0330

    feat: integrate better-auth for social login with Discord
    - Added better-auth and prisma client dependencies in package.json.
    - Created initial Prisma migration for user, session, account, and verification tables.
    - Implemented auth logic using better-auth with Discord as a social provider.
    - Added auth client for handling social login in the application.
    - Updated SocialLogin component to initiate Discord login.

--- src/components/auth/login/SocialLogin.tsx
+++ src/components/auth/login/SocialLogin.tsx
@@ -1,13 +1,22 @@
+"use client";
 import React from "react";
 import WideButton from "../shared/WideButton";
 import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
 
 const SocialLogin: React.FC = () => {
+  const discordLoginOnClick = async () => {
+    await authClient.signIn.social({
+      provider: "discord",
+      callbackURL: "/",
+    });
+  };
   return (
     <div className="w-full flex">
       <WideButton
         type="button"
         text="ورود با دیسکورد"
+        onClick={discordLoginOnClick}
         extendedClassName="bg-[#5865F2] hover:bg-[#4752C4]"
       >
         <Image src="/discord.svg" alt="Discord Logo" width={26} height={26} />
---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  socialProviders: {
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
