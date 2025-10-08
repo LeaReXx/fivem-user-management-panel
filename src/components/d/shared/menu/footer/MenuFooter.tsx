aba7637c5a17b6bd1c970a85a24576c57c2d9068
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-10-08 13:43:40 +0330

    fix logout btn action

--- src/components/d/shared/menu/footer/MenuFooter.tsx
+++ src/components/d/shared/menu/footer/MenuFooter.tsx
@@ -1,15 +1,30 @@
 import { LogOut } from "lucide-react";
 import UserProfile from "../../user profile/UserProfile";
-("react");
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 const MenuFooter: React.FC = () => {
+  const router = useRouter();
+
+  const onClickLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  };
   return (
     <div className="flex justify-center md:justify-normal items-end w-full h-[15%] pb-4">
       <div className="hidden lg:block">
         <UserProfile />
       </div>
 
-      <button className="flex cursor-pointer lg:hidden items-center gap-6 text-white opacity-80 bg-red-600 hover:bg-red-700 p-3 rounded-lg transition-colors w-full">
+      <button
+        onClick={onClickLogout}
+        className="flex cursor-pointer lg:hidden items-center gap-6 text-white opacity-80 bg-red-600 hover:bg-red-700 p-3 rounded-lg transition-colors w-full"
+      >
         <LogOut size={20} />
         <span>خروج از حساب</span>
       </button>
