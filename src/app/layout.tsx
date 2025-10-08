2d46d85fa1e2c9d627ec2eadf57b6f05de505fce
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-10-08 13:50:04 +0330

    fix height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${IRANSans.className} h-screen relative bg-gray-400 dark:bg-zinc-900 bg-fixed bg-cover bg-center bg-no-repeat antialiased`}
+        className={`${IRANSans.className} min-h-[100dvh] relative bg-gray-400 dark:bg-zinc-900 bg-fixed bg-cover bg-center bg-no-repeat antialiased`}
         style={{
           backgroundImage: "var(--custom-background-image)",
         }}
