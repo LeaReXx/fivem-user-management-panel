b74e3ba6b07a4fa418874ceb9dee90ebd9199921
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-10-14 01:55:36 +0330

    removal: remove shiki dependency and related components
    - Remove shiki package from package.json
    - Remove ShikiEditor component from src/app/(dashboard)/d/shop/page.tsx
    - Remove various dev dependencies related to markdown processing and syntax highlighting

--- src/app/(dashboard)/d/shop/page.tsx
+++ src/app/(dashboard)/d/shop/page.tsx
@@ -1,10 +1,7 @@
-import ShikiEditor from "@/components/d/ShikiEditor";
 import React from "react";
 
 const ShopPage: React.FC = () => {
-  return <div className="p-6 w-full">
-    <ShikiEditor/>
-  </div>;
+  return <div className="p-6 w-full"></div>;
 };
 
 export default ShopPage;
