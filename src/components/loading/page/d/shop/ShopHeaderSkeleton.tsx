96a90ef9ed949d7d6fba184dbaf40451178d14ad
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-10-22 15:42:46 +0330

    style(skeleton): adjust width of shop header skeleton
    - Change max-h to w in Skeleton component for shop header
    - Update width value from max-w-[180px] to w-[180px]
    - Remove unnecessary sm: prefix for width class

--- src/components/loading/page/d/shop/ShopHeaderSkeleton.tsx
+++ src/components/loading/page/d/shop/ShopHeaderSkeleton.tsx
@@ -6,7 +6,7 @@ const ShopHeaderSkeleton = () => {
   return (
     <div className="flex flex-col gap-4 sm:gap-0 sm:justify-between sm:items-center sm:flex-row md:w-2/3">
       <Skeleton className="sm:w-1/2 sm:max-w-[320px] h-[50px]" />
-      <Skeleton className="max-h-[180px] h-[50px]" />
+      <Skeleton className="w-[180px] h-[50px]" />
     </div>
   );
 };
