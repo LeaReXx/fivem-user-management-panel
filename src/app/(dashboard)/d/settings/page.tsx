17ef265cd999c46267ce99178a8ebb09fb12047b
Sepehr Aghapoor <dev.sepehr@outlook.com>
2025-10-27 13:09:48 +0330

    complete user detail box in settings page

--- src/app/(dashboard)/d/settings/page.tsx
+++ src/app/(dashboard)/d/settings/page.tsx
@@ -1,7 +1,16 @@
+import UserDetail from "@/components/d/settings/user detail/UserDetail";
+import PageHeader from "@/components/d/shared/page header/PageHeader";
 import React from "react";
 
 const SettingsPage: React.FC = () => {
-  return <div className="p-6"></div>;
+  return (
+    <div className=" bg-linear-to-t from-content-box-bg-color-1 to-content-box-bg-color-2 backdrop-blur-[2px] rounded-lg p-4 min-h-[500px]">
+      <PageHeader title="تنظیمات" iconName="settings" />
+      <div className="mt-4 grid grid-cols-12 gap-4">
+        <UserDetail />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
