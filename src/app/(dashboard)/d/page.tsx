2d35d72b91031fedadfa0144c78b6b61c2e645cc
Sepehr Aghapoor <dev.sepehr@outlook.com>
2025-10-28 23:53:52 +0330

    Remove React import and reorder dashboard imports

--- src/app/(dashboard)/d/page.tsx
+++ src/app/(dashboard)/d/page.tsx
@@ -1,7 +1,6 @@
-import UserNotifications from "@/components/d/dashboard/notifications/UserNotifications";
 import FeaturedProducts from "@/components/d/dashboard/featured products/FeaturedProducts";
+import UserNotifications from "@/components/d/dashboard/notifications/UserNotifications";
 import UserSubscriptionStatus from "@/components/d/dashboard/subscription status/UserSubscriptionStatus";
-import React from "react";
 
 const DashboardPage: React.FC = () => {
   return (
