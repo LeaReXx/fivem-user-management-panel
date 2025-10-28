de6db5bb58ecb49b0c013481a93ea9b06bc4624d
Sepehr Aghapoor <dev.sepehr@outlook.com>
2025-10-28 18:55:35 +0330

    Use next/image in UserDetail and improve layout

--- src/components/d/settings/user detail/UserDetail.tsx
+++ src/components/d/settings/user detail/UserDetail.tsx
@@ -2,12 +2,12 @@
 import { User } from "lucide-react";
 import Input from "@/components/ui/Input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import VerifyAccountModal from "./verify account modal/VerifyAccountModal";
 import { useQueryState } from "nuqs";
+import Image from "next/image";
 
 const UserDetail = () => {
-  const [,setIsOpenVerifyModal] = useQueryState("verify");
+  const [, setIsOpenVerifyModal] = useQueryState("verify");
 
   const handleOpenVerifyModlal = () => {
     setIsOpenVerifyModal("open");
@@ -16,7 +16,7 @@ const UserDetail = () => {
   return (
     <>
       <VerifyAccountModal />
-      <div className="bg-inside-box-bg-color col-span-12 md:col-span-6 rounded-lg p-4">
+      <div className="bg-inside-box-bg-color col-span-12 xl:col-span-6 rounded-lg p-4">
         <div className="pb-4 flex justify-between items-center">
           <p className="flex gap-2 font-medium">
             <User size={22} strokeWidth={1.5} /> اطلاعات کاربر
@@ -27,16 +27,20 @@ const UserDetail = () => {
               onClick={handleOpenVerifyModlal}
               className="bg-linear-to-t from-blue-500/90 to-blue-500/60"
             >
-                تایید حساب کاربری
+              تایید حساب کاربری
             </Button>
           </div>
         </div>
-        <div className="flex flex-col justify-center gap-4 w-full items-center max-w-[400px] mx-auto xl:flex-row xl:max-w-none xl:items-start">
-          <div className="w-full mx-auto max-w-[250px] xl:max-w-[150px] 2xl:max-w-[180px] bg-yellow-500/50 p-1 rounded-md">
+        <div className="flex flex-col justify-center gap-4 w-full items-start max-w-[600px] mx-auto sm:flex-row xl:max-w-none xl:flex-col 2xl:flex-row xl:items-start">
+          <div className="w-full mx-auto max-w-[250px] sm:max-w-[200px] xl:max-w-[250px] 2xl:max-w-[180px] bg-yellow-500/50 p-1 rounded-md">
             <div className="aspect-square w-full">
-              <img
+              <Image
                 src="/profile.jpg"
                 className="size-full object-cover rounded-sm"
+                alt="profile"
+                width={200}
+                height={200}
+                unoptimized
               />
             </div>
             <div className="py-1 px-2 text-center w-full">
@@ -45,16 +49,26 @@ const UserDetail = () => {
           </div>
           <div className="space-y-4 w-full">
             <div>
-              <label>نام</label>
-              <Input value="سپهر آقاپور" icon="user" disabled />
+              <label htmlFor="full-name">نام</label>
+              <Input
+                value="سپهر آقاپور"
+                icon="user"
+                name="full-name"
+                disabled
+              />
             </div>
             <div>
-              <label>ایمیل</label>
-              <Input value="petrol_cs@yahoo.com" icon="mail" disabled />
+              <label htmlFor="email">ایمیل</label>
+              <Input
+                value="petrol_cs@yahoo.com"
+                name="email"
+                icon="mail"
+                disabled
+              />
             </div>
             <div>
-              <label>تلفن</label>
-              <Input value="09353195043" icon="phone" disabled />
+              <label htmlFor="phone">تلفن</label>
+              <Input value="09353195043" icon="phone" name="phone" disabled />
             </div>
           </div>
         </div>
