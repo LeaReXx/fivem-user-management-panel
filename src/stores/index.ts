ad2f7a4e51ce6e945979a08ff7df3af70a962c1a
Sepehr Aghapoor <dev.sepehr@outlook.com>
2025-10-30 15:28:09 +0330

    Export social accounts from discord-account-store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,6 @@
 // Export all stores from here for easy imports
 export { useNavbarStore } from "./navbar-store";
-export { useSocialAccountsStore } from "./social-accounts-store";
+export { useSocialAccountsStore } from "./discord-account-store";
 
 // Example for future stores:
 // export { useAuthStore } from './auth-store'
