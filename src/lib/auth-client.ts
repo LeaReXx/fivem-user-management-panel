997c20d8a1318a0a7ded65ef48aab2909638b197
Sepehr Aghapoor <dev.sepehr@outlook.com>
2025-11-01 15:56:51 +0330

    export authClient Session type

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,3 +2,4 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL as string,
 });
+export type Session = typeof authClient.$Infer.Session;
